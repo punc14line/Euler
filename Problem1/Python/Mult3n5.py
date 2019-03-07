e6b0c2168a415b7ecf2d0b1508cabbc8ef64d028
punch14line <matthew.hacker14@gmail.com>
2019-03-07 13:41:38 -0800

    Python solution

---
+++ Problem1/Python/Mult3n5.py
@@ -0,0 +1,17 @@
+def Sumof3n5(max):
+    sum = 0
+    i = 3
+    
+    while i < max:
+        sum = sum + i
+        i = i + 3
+    
+    i = 5
+    while i < max:
+        if i % 3 != 0:
+            sum = sum + i
+        i = i + 5
+    
+    return sum
+
+print Sumof3n5(1000)
